354f4824bd06b588ffe4fbef8f3224912e21a92e
George Mihov <gmihov001@gmail.com>
2021-03-24 21:01:39 +0000

    Updates

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useContext} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import {Context} from "../store/appContext";
 
-export const Home = () => (
+export const Home = () => {
+const { store, actions } = useContext(Context);
+return (
+
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
-		<button href="#" className="btn btn-success">
+		<button href="#" className="btn btn-success" onClick={()=> actions.sendFunnyWord()}>
 			Add a funny word
 		</button>
 	</div>
 );
+}
